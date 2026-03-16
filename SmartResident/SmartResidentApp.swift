48f5c95cc1b5f5d5d38efc8de234b0a85a5e3e64
Diana Alejandra Pava Avila <dianapava65@gmail.com>
2026-03-16 10:09:46 -0500

    Initial Commit

---
+++ SmartResident/SmartResidentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartResidentApp.swift
+//  SmartResident
+//
+//  Created by Diana Alejandra Pava A on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartResidentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
